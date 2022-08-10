17afd3f46f79db74dcc88118c3fdd5f95a5b21ff
Jamie Moreland <jamie1964two@hotmail.co.uk>
2022-08-10 13:17:25 +0100

    Initialise repository for Node/Express course

---
+++ starter/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const tasks = require('./public/routes/tasks');
+
+// middleware
+
+app.use(express.json())
+
+
+
+// routes
+app.get('/hello',  (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
+
+
